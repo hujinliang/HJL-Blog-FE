7946bd879212b59d01c734f2fdd6db6edc48f98c
hujingliang <1617451312@qq.com>
2016-08-12 12:49:04 +0800

    '0812'

--- src/components/Article/comment.js
+++ src/components/Article/comment.js
@@ -90,9 +90,59 @@ export default class Comment extends React.Component{
         const {commentList,auth,submitComment,openLoginModal} = this.props
         return (
             <div className="comment-container clearfix">
-                <div className="comment-head clearfix">
+                { auth.token ?
+
+                    <div className="comment-reply">
+                        <a className="reply-avatar" href="javascript:;">
+                            {auth&&auth.user? <img src={auth.user.avatar || defaultAvatar}/>
+                                :
+                                <img src={ defaultAvatar}/>
+                            }
+                        </a>
+                        <form className="comment-form" onSubmit={this.handleSubmit(submitComment)}>
+                            <div className="comment-content">
+                                <textarea
+                                    maxLength="2000"
+                                    required
+                                    placeholder="写下你的评论…"
+                                    onChange={this.handleCommentContentChange}
+                                    id="comment_content"  value={this.state.commentContent}>
+
+                                </textarea>
+                            </div>
+                            <div className="button-container clearfix">
+                                <input type="submit" id="comment_submit_btn" value="发 表" className="btn btn-info pull-right" />
+                                <button className="btn btn-danger pull-right" onClick={this.clearText}>清空</button>
+
+                            </div>
+                        </form>
+                    </div>
+
+
+                    // <form className="new_comment" onSubmit={this.handleSubmit(submitComment)}>
+                    //     <div className="comment-text">
+                    //     <textarea
+                    //         maxLength="2000"
+                    //         required
+                    //         placeholder="写下你的评论…"
+                    //         onChange={this.handleCommentContentChange}
+                    //         id="comment_content"  value={this.state.commentContent}>
+                    //
+                    //     </textarea>
+                    //         <div>
+                    //             <input type="submit" id="comment_submit_btn" value="发 表" className="btn btn-info" />
+                    //         </div>
+                    //     </div>
+                    // </form>
+                    :
+                    <div>
+                        <p className="comment-signin">
+                            <button className="btn btn-info" onClick={openLoginModal}>登录后发表评论</button>
+                        </p>
+                    </div>
+                }
+                <div className="comment-des clearfix">
                     {commentList.items.length || 0}条评论
-                    <a href="javascript:;" className="goto-comment pull-right"><i className="fa fa pencil"></i>添加评论</a>
                 </div>
                 <div id="comment_list">
                     {commentList.items.map((comment,i) =>
@@ -129,57 +179,64 @@ export default class Comment extends React.Component{
                             </div>
                     )}
                 </div>
-                { auth.token ?
 
-                    <div className="comment-reply">
-                        <a className="reply-avatar" href="javascript:;">
-                        {auth&&auth.user? <img src={auth.user.avatar || defaultAvatar}/>
-                        :
-                            <img src={ defaultAvatar}/>
-                        }
-                        </a>
-                        <form className="comment-form" onSubmit={this.handleSubmit(submitComment)}>
-                            <div className="comment-content">
-                                <textarea
-                                maxLength="2000"
-                                required
-                                placeholder="写下你的评论…"
-                                onChange={this.handleCommentContentChange}
-                                id="comment_content"  value={this.state.commentContent}>
-
-                                </textarea>
+                <ul className="comments">
+                    {commentList.items.map((comment,i) =>
+                        <li className="comments-item" key={i}>
+                            <div className="comment-self">
+                                <div className="comment-selt-avatar">
+                                    <img src={comment.user_id.avatar || defaultAvatar} alt=""/>
+                                </div>
+                                <div className="comment-self-body">
+                                    <div className="comment-self-header">
+                                        <span>{comment.user_id.nickname}</span>
+                                    </div>
+                                    <p className="comment-self-content">{comment.content}</p>
+                                    <div className="comment-self-footer">
+                                        <span className="commment-time">{formatDate(comment.created)}</span>
+                                        <a href="javascript:;" className="comment-reply" onClick={e=>this.showReply(e,i,comment.user_id.nickname)}>
+                                            <i className="fa fa-mail-reply"> </i>
+                                            回复
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="button-container clearfix">
-                                <input type="submit" id="comment_submit_btn" value="发 表" className="btn btn-info pull-right" />
-                                <button className="btn btn-danger pull-right" onClick={this.clearText}>清空</button>
-                                
+                            <Reply replys={comment.replys} k={i} showReply={this.showReply}/>
+                            
+                            
+                            <div className="content">
+                                <div className="meta-top">
+                                    <a className="avatar">
+                                        <img src={comment.user_id.avatar || defaultAvatar} alt={comment.user_id.nickname} />
+                                    </a>
+                                    <a className="author-name link-light">{comment.user_id.nickname}</a>
+                                        <span className="reply-time">
+                                            {formatDate(comment.created)}
+                                        </span>
+                                </div>
+                                <p className="comment-content">{comment.content}</p>
+                                <div className="comment-footer text-right">
+                                    <a className="reply" href="javascript:;" onClick={e=>this.showReply(e,i,comment.user_id.nickname)}>回复</a>
+                                </div>
+                                <Reply replys={comment.replys} k={i} showReply={this.showReply}/>
+
+                                <form className="new-reply hide" ref={'reply_form_'+i} id={'reply_form_'+i} onSubmit={e=>this.handleSubmitReply(e,i,comment._id)}>
+                                    <div className="comment-text">
+                                         <textarea id={'replyContent'+i}
+                                                   maxLength="2000"
+                                                   ref={'replyContent'+i}
+                                                   placeholder="写下你的回复…"> </textarea>
+                                        <div>
+                                            <input type="submit" value="发 表" className="btn btn-sm btn-info" />
+                                        </div>
+                                    </div>
+                                </form>
+
                             </div>
-                        </form>
-                    </div>
+                        </li>
+                    )}
+                </ul>
 
-
-                    // <form className="new_comment" onSubmit={this.handleSubmit(submitComment)}>
-                    //     <div className="comment-text">
-                    //     <textarea
-                    //         maxLength="2000"
-                    //         required
-                    //         placeholder="写下你的评论…"
-                    //         onChange={this.handleCommentContentChange}
-                    //         id="comment_content"  value={this.state.commentContent}>
-                    //
-                    //     </textarea>
-                    //         <div>
-                    //             <input type="submit" id="comment_submit_btn" value="发 表" className="btn btn-info" />
-                    //         </div>
-                    //     </div>
-                    // </form>
-                    :
-                    <div>
-                        <p className="comment-signin">
-                            <button className="btn btn-info" onClick={openLoginModal}>登录后发表评论</button>
-                        </p>
-                    </div>
-                }
             </div>
         )
     }
--- src/components/Article/reply.js
+++ src/components/Article/reply.js
@@ -3,27 +3,57 @@
  */
 import React from 'react'
 import {formatDate} from '../../utiles'
+import defaultAvatar from '../../assets/imgs/userimg.png'
 
 export default class Reply extends React.Component{
  render(){
      const {replys,k,showReply} = this.props;
      console.log(replys)
      return (
-         <div className="reply-list">
+
+         <div className="replys">
              {replys.map((reply,i) => (
-                 <div className="reply-item" key={i}>
-                     <p className="reply-content">
-                         <a className="reply-user link-light">{reply.user_info.nickname}</a>
-                         {reply.content}
-                     </p>
-                     <div className="reply-footer text-right">
-                         <a href="javascript:;" className="reply" onClick={e=>showReply(e,k,reply.user_info.nickname)}>回复</a>
-                         <span className="reply-time pull-left">{formatDate(reply.created)}</span>
+                 <div className="replys-item" key={i}>
+                     <div className="reply-avatar">
+                         <a href="javascript:;">
+                             <img src={reply.user_info.avatar || defaultAvatar} alt=""/>
+                         </a>
                      </div>
+                     <div className="reply-body">
+                         <div className="reply-header">
+                             <span>{reply.user_info.nickname}</span>
+                         </div>
+                         <p className="reply-content">{reply.content}</p>
+                         <div className="reply-footer">
+                             <span className="reply-time">{formatDate(reply.created)}</span>
+                             <a href="javascript:;" className="reply-reply" onClick={e=>showReply(e,k,reply.user_info.nickname)}>
+                                 <i className="fa fa-mail-reply"> </i>
+                                 回复
+                             </a>
+                         </div>
+                     </div>
+
                  </div>
              ))
              }
          </div>
+
+
+         // <div className="reply-list">
+         //     {replys.map((reply,i) => (
+         //         <div className="reply-item" key={i}>
+         //             <p className="reply-content">
+         //                 <a className="reply-user link-light">{reply.user_info.nickname}</a>
+         //                 {reply.content}
+         //             </p>
+         //             <div className="reply-footer text-right">
+         //                 <a href="javascript:;" className="reply" onClick={e=>showReply(e,k,reply.user_info.nickname)}>回复</a>
+         //                 <span className="reply-time pull-left">{formatDate(reply.created)}</span>
+         //             </div>
+         //         </div>
+         //     ))
+         //     }
+         // </div>
      )
  }
 }
